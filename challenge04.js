e30e5d318c355675d8e7814d30113d888728bf1d
Ger <gerfersie@gmail.com>
2022-12-01 19:05:30 +0100

    Challenge 04-05

---
+++ challenge04.js
@@ -0,0 +1,27 @@
+const minRange = 11098
+const maxRange = 98123
+
+const main = () => {
+  const numbers = []
+  for (let i = minRange; i <= maxRange; i++) {
+    numbers.push(i)
+  }
+
+  const answers = numbers.filter(isAscending).filter(haveTwoFive)
+
+  console.log(`Lenght ${answers.length}`)
+  console.log(`password ${answers[55]}`)
+}
+
+// 12345
+const isAscending = (numero) => {
+  const convertToNumber = (num) => Number(num)
+  const arr = Array.from(String(numero)).map(convertToNumber)
+  return arr.every((n, i) => i === 0 || n >= arr[i - 1])
+}
+
+// haveFive(465) -> false
+// haveFive(255) -> true
+const haveTwoFive = (num) => `${num}`.includes('55')
+
+main()
---
+++ challenge05.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+
+const result = async () => {
+    const resp = await axios('https://codember.dev/mecenas.json')
+    const mecenas = await resp.data
+    
+    const players = mecenas.map((mecena, idx) => {
+      return { name: mecena, idx, is_alive: true }
+    })
+    
+    const hungerGames = (players) => {
+      const idxPlayers = [...players.keys()]
+    
+      while (players.length !== 1) {
+        // si no es par automaticamente eliminado
+        players.forEach((p, i) => {
+          if (idxPlayers[i] % 2 !== 0) {
+            players[i].is_alive = false
+          }
+        })
+    
+        // si el ultimo esta vivo y el primero tambien
+        // el primero se va a un lugar mejor
+        if (players.at(-1).is_alive && players.at(0).is_alive) {
+          players[0].is_alive = false
+        }
+    
+        // se quitan del array los jugadores eliminados
+        players = players.filter((p) => p.is_alive)
+      }
+    
+      return players
+    }
+    
+    console.log(hungerGames(players))
+}  
+
+result();
